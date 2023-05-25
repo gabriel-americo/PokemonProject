6d3910c2c81c257e4c03ec43bbca30d841769e12
Gabriel Americo <gabrielamerico90@gmail.com>
2023-05-25 17:05:59 -0300

    Pokemon API Project01

---
+++ Interfaces/IReviewerRepository.cs
@@ -0,0 +1,12 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface IReviewerRepository
+    {
+        ICollection<Reviewer> GetReviwers();
+        Reviewer GetReviwer(int reviewerId);
+        ICollection<Review> GetReviwesByReviers(int reviewerId);
+        bool ReviwerExists(int reviewerId);
+    }
+}
---
+++ Repository/ReviewerRepository.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class ReviewerRepository : IReviewerRepository
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public ReviewerRepository(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public Reviewer GetReviwer(int reviewerId)
+        {
+            return _context.Reviewers.Where(r => r.Id == reviewerId).Include(e => e.Reviews).FirstOrDefault();
+        }
+
+        public ICollection<Reviewer> GetReviwers()
+        {
+            return _context.Reviewers.ToList();
+        }
+
+        public ICollection<Review> GetReviwesByReviers(int reviewerId)
+        {
+            return _context.Reviews.Where(r => r.Reviewer.Id == reviewerId).ToList();
+        }
+
+        public bool ReviwerExists(int reviewerId)
+        {
+            return _context.Reviewers.Any(r => r.Id == reviewerId);
+        }
+    }
+}
